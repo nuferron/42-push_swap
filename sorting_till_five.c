bc51faa8e9ddf84e15c77eaccfd87254db9f099c
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2023-08-12 20:34:40 +0200

    I haven't really changed this

--- sorting_till_five.c
+++ sorting_till_five.c
@@ -6,7 +6,7 @@
 /*   By: nuferron <nuferron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 13:26:34 by nuferron          #+#    #+#             */
-/*   Updated: 2023/08/09 15:08:19 by nuferron         ###   ########.fr       */
+/*   Updated: 2023/08/11 22:26:05 by nuferron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,32 +20,32 @@ void	sorting_three(t_stack **a)
 	{
 		last = last_node(*a);
 		if ((*a)->id - (*a)->next->id == 1)
-			swap(a, 's', 'a');
+			swap(a, 'a');
 		else if ((*a)->id - last->id == 1 || (*a)->id - last->id == -1)
-			reverse_rotate(a, 'v', 'a');
+			reverse_rotate(a, 'a');
 		else
-			rotate(a, 'r', 'a');
+			rotate(a, 'a');
 	}
 }
 
 void	sorting_four(t_stack **a, t_stack **b, t_stack *last)
 {
 	if ((*a)->id == 0 || (*a)->id == 3)
-		push(a, b, 'p', 'b');
+		push(a, b, 'b');
 	else if (last->id == 0 || last->id == 3)
 	{
-		reverse_rotate(a, 'v', 'a');
-		push(a, b, 'p', 'b');
+		reverse_rotate(a, 'a');
+		push(a, b, 'b');
 	}
 	else
 	{
-		swap(a, 's', 'a');
-		push(a, b, 'p', 'b');
+		swap(a, 'a');
+		push(a, b, 'b');
 	}
 	sorting_three(a);
-	push(b, a, 'p', 'a');
+	push(b, a, 'a');
 	if ((*a)->id == 3)
-		rotate(a, 'r', 'a');
+		rotate(a, 'a');
 }
 
 void	five_last_sort(t_stack **a, t_stack **b)
@@ -53,15 +53,15 @@ void	five_last_sort(t_stack **a, t_stack **b)
 	int	max;
 
 	max = is_max(*a);
-	push(b, a, 'p', 'a');
-	push(b, a, 'p', 'a');
+	push(b, a, 'a');
+	push(b, a, 'a');
 	if (is_sorted(*a) == 0)
 		return ;
 	if (((*a)->next->id > max / 2 && (*a)->id > (*a)->next->id)
 		|| ((*a)->next->id > max / 2 && (*a)->id - (*a)->next->id != 1))
-		swap(a, 's', 'a');
+		swap(a, 'a');
 	while (is_sorted(*a) != 0)
-		rotate(a, 'r', 'a');
+		rotate(a, 'a');
 }
 
 void	sorting_five(t_stack **a, t_stack **b)
@@ -74,13 +74,13 @@ void	sorting_five(t_stack **a, t_stack **b)
 		min = is_min(*a);
 		max = is_max(*a);
 		if ((*a)->id == min || (*a)->id == max)
-			push(a, b, 'p', 'b');
+			push(a, b, 'b');
 		else if ((*a)->next->id == min || (*a)->next->id == max)
-			swap(a, 's', 'a');
+			swap(a, 'a');
 		else if ((*a)->next->next->id == min || (*a)->next->next->id == max)
-			rotate(a, 'r', 'a');
+			rotate(a, 'a');
 		else 
-			reverse_rotate(a, 'v', 'a');
+			reverse_rotate(a, 'a');
 	}
 	sorting_three(a);
 	five_last_sort(a, b);
