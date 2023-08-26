530e575a90d8e4edf48979a7e93f6d88892d6821
nuferron <nuriaf207@gmail.com>
2023-08-26 21:24:19 +0200

    removed protections
    the protections where the cause of the "Error" output instead of the "KO" output I should have displayed instead

--- ps_bonus/checker_do.c
+++ ps_bonus/checker_do.c
@@ -14,18 +14,18 @@
 
 int	do_swap(t_stack **a, t_stack **b, char *str)
 {
-	if (ft_strncmp(str, "sa\n", 3) == 0 && element_num(*a) > 1)
+	if (ft_strncmp(str, "sa\n", 3) == 0)
 	{
 		swap(a, '0');
 		return (0);
 	}
-	if (ft_strncmp(str, "sb\n", 3) == 0 && element_num(*b) > 1)
+	if (ft_strncmp(str, "sb\n", 3) == 0)
 	{
 		swap(b, '0');
 		return (0);
 	}
 	if (ft_strncmp(str, "ss\n", 3) == 0
-		&& element_num(*a) > 1 && element_num(*b) > 1)
+		&& element_num(*a) > 1)
 	{
 		swap(a, '0');
 		swap(b, '0');
@@ -36,25 +36,25 @@ int	do_swap(t_stack **a, t_stack **b, char *str)
 
 int	do_rr(t_stack **a, t_stack **b, char *str)
 {
-	if (ft_strncmp(str, "rra\n", 4) == 0 && element_num(*a) > 1)
+	if (ft_strncmp(str, "rra\n", 4) == 0)
 	{
 		reverse_rotate(a, '0');
 		return (0);
 	}
-	if (ft_strncmp(str, "rrb\n", 4) == 0 && element_num(*b) > 1)
+	if (ft_strncmp(str, "rrb\n", 4) == 0)
 	{
 		reverse_rotate(b, '0');
 		return (0);
 	}
 	if (ft_strncmp(str, "rrr\n", 4) == 0
-		&& element_num(*a) > 1 && element_num(*b) > 1)
+		&& element_num(*a) > 1)
 	{
 		reverse_rotate(a, '0');
 		reverse_rotate(b, '0');
 		return (0);
 	}
 	if (ft_strncmp(str, "rr\n", 3) == 0
-		&& element_num(*a) > 1 && element_num(*b) > 1)
+		&& element_num(*a) > 1)
 	{
 		rotate(a, '0');
 		rotate(b, '0');
@@ -65,12 +65,12 @@ int	do_rr(t_stack **a, t_stack **b, char *str)
 
 int	do_rot(t_stack **a, t_stack **b, char *str)
 {
-	if (ft_strncmp(str, "ra\n", 3) == 0 && element_num(*a) > 1)
+	if (ft_strncmp(str, "ra\n", 3) == 0)
 	{
 		rotate(a, '0');
 		return (0);
 	}
-	if (ft_strncmp(str, "rb\n", 3) == 0 && element_num(*b) > 1)
+	if (ft_strncmp(str, "rb\n", 3) == 0)
 	{
 		rotate(b, '0');
 		return (0);
@@ -80,12 +80,12 @@ int	do_rot(t_stack **a, t_stack **b, char *str)
 
 int	do_push(t_stack **a, t_stack **b, char *str)
 {
-	if (ft_strncmp(str, "pa\n", 3) == 0 && element_num(*b) >= 1)
+	if (ft_strncmp(str, "pa\n", 3) == 0)
 	{
 		push(b, a, '0');
 		return (0);
 	}
-	if (ft_strncmp(str, "pb\n", 3) == 0 && element_num(*a) >= 1)
+	if (ft_strncmp(str, "pb\n", 3) == 0)
 	{
 		push(a, b, '0');
 		return (0);
